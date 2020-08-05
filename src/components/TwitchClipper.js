a9c4e16baa11d3ee8d9ebb5b97242fb1a86e774a
dstollbyu <elderstoll@gmail.com>
2020-08-05 11:39:41 -0600

    Fixed styling on medofficetemplate, finished base form of twitch clipper

--- src/components/TwitchClipper.js
+++ src/components/TwitchClipper.js
@@ -9,30 +9,29 @@ const TwitchClipper = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
   // State hook to hold the list of clips
   const [clips, setClips] = React.useState([]);
+  // State hook to handle the url of the API to grab the userId
+  const [url, setUrl] = React.useState('https://api.twitch.tv/helix/users?login=')
 
-  // onSearchSubmit function that does the work when "Submit" is clicked
-  const onSearchSubmit = (event) => {
-    
-    // Reset the clips array, please? https://overreacted.io/a-complete-guide-to-useeffect/
-    setClips([]);
+  // useEffect hook that does the work when "Submit" is clicked and the url changes
+  React.useEffect(() => {
+    if (url !== 'https://api.twitch.tv/helix/users?login=') {
+      // Defines config variable to be used for retrieving user id
+      let configUserId = {
+          method: 'get',
+          url: url,
+          headers: { 
+            'client-id': '2zrys8su2u1tm0yip0adcvx2s7czr2', 
+            'Authorization': process.env.REACT_APP_TWITCH_AUTH
+        }
+      };
 
-    // Defines config variable to be used for retrieving user id
-    let configUserId = {
-        method: 'get',
-        url: `https://api.twitch.tv/helix/users?login=${searchTerm}`,
-        headers: { 
-          'client-id': '2zrys8su2u1tm0yip0adcvx2s7czr2', 
-          'Authorization': process.env.REACT_APP_TWITCH_AUTH
-      }
-    };
-
-    // Use axios to return a JSON response with clip information based on the user id
-    axios(configUserId)
-      .then((response) => {
+      // Asynchronous function to handle the axios calls
+      const fetchData = async () => {
+        setClips([]);
         let userId;
-        // Store the userId into a variable from the retrieved json data
-        let userData = response.data.data;
-        if (userData.length !== 0) userId = response.data.data[0].id;
+        // Use axios to return a JSON response with the user id based on the username entered
+        const responseUserId = await axios(configUserId);
+        if (responseUserId.data.data.length !== 0) userId = responseUserId.data.data[0].id;
         // If userId is defined, then pull the list of clips
         if (userId !== undefined) {
           // Defines config variable to be used for retrieving clip info from user id
@@ -44,62 +43,60 @@ const TwitchClipper = () => {
               'Authorization': process.env.REACT_APP_TWITCH_AUTH
             }
           }
-          // Another axios call to return the actual clip information
-          axios(configClipInfo)
-            .then((response) => {
-              const responseList = response.data.data;
-              setClips([...clips, ...responseList]);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        } else {
-          console.log("UserID does not exist");
+          // Use axios to return a JSON response with clip information based on the user id
+          const responseList = await axios(configClipInfo);
+          // setClips to that information
+          setClips(responseList.data.data);
         }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    // Prevents browser from reloading when search is submitted
-    event.preventDefault();
-  }
-
-  // handleSearchInput function to set searchTerm equal to the value in the input every time it's changed; necessary for onChange to exist
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
-  }
+      }
+      fetchData();
+    }
+  }, [url]);
 
   return (
     <div className="clipBody">
       <NavbarProj />
       <div className="clipContainer">
-        <form onSubmit={onSearchSubmit} className="search">
-          <p>Enter a username:</p>
-          <input id="search" isfocused="true" value={searchTerm} onChange={handleSearchInput} />
-          <button type="submit" disabled={!searchTerm}>Submit</button>
-        </form>
-        <div className="clipListContainer">
-          <List clipsList={clips} />
-        </div>
+        <SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} setUrl={setUrl} />
+        <List clipsList={clips} />
       </div>
     </div>
   )
 }
 
-const List = ({ clipsList }) => {
-  console.log(clipsList);
+const SearchForm = ({ searchTerm, setSearchTerm, setUrl }) => {
   return (
-    <>
+    <form onSubmit={event => {setUrl(`https://api.twitch.tv/helix/users?login=${searchTerm}`); event.preventDefault();}} className="search">
+      <p>Enter a username:</p>
+      <input id="search" isfocused="true" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
+      <button type="submit" disabled={!searchTerm}>Submit</button>
+    </form>
+  );
+};
+
+// List component that holds the Twitch clips list
+const List = ({ clipsList }) => {
+  // console.log(clipsList);
+  return (
+    <div className="clipListContainer">
       {clipsList.map((item) => (<Item key={item.id} item={item} />))}
-    </>
+    </div>
   )
 }
 
+// Item component that renders the properties of each clip
 const Item = ({ item }) => {
   return (
     <>
-      <p> {item.url} - {item.title} </p>
+      <a href={item.url} target="_blank" rel="noreferrer noopener">
+        <div className="twitchClipItem">
+          <span style={{width: '10%'}}><img className="twitchThumbnail" src={item.thumbnail_url} alt="" /> </span>
+          <span style={{width: '40%'}}>{item.title}</span>
+          <span style={{width: '20%', textAlign:'center'}}>{item.broadcaster_name}</span>
+          <span style={{width: '20%', textAlign:'center'}}>{item.creator_name}</span>
+          <span style={{width: '10%'}}>{item.view_count}</span>
+        </div>
+      </a>
     </>
   )
 }
